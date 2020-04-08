b9c7d31427f281dd70551c170293a620b47caf80
Alberto Monge <alberto.monge@polito.it>
2020-04-08 09:37:06 +0200

    progetto base

---
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -0,0 +1,14 @@
+package it.polito.tdp.nobel.model;
+
+import java.util.List;
+
+public class Model {
+
+	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		
+		System.out.println("TODO!");
+		
+		return null;
+	}
+
+}
